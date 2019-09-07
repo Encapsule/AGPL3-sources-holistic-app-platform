df95dc409ac4fb9ec256c2f36557b1203b6ce451
Chris Russell <cdr@encapsule.io>
2019-09-06 18:04:27 -0700

    WIP: We need to improve the data gateway to ensure that each... ... data gateway filter can specify its result and error formats via filter specifications just an @encapsule/holism service filter. What I think is going to happen is that I'm going to move the data gateway library components out of the holarchy library and add them into the @encapsule/holism library itself to make it simpler to gain access to the internal filter specifications used by service filters.

--- SOURCES/LIB/holarchy/server/services/data-gateway/data-gateway-filter-factory.js
+++ SOURCES/LIB/holarchy/server/services/data-gateway/data-gateway-filter-factory.js
@@ -1,9 +1,6 @@
 // sources/server/services/rainier-ux-data/data-gateway-filter-factory.js
-
-// This module exports a filter that implements a factory that
-// builds special-purpose filters that receive specific incoming
-// client GET/POST request messages from the Node.js app server's
-// service-rainier-ux-data.js module.
+//
+// This module exports a filter that implements a factory for building data gateway filters.
 
 const arccore = require("@encapsule/arccore");
 
@@ -27,8 +24,22 @@ var factoryResponse = arccore.filter.create({
             ____accept: "jsString"
         },
         gatewayMessageSpec: {
-            ____label: "Data Gateway Message Specification",
+            ____label: "Data Gateway Filter Request Message Specification",
             ____accept: "jsObject"
+        },
+        gatewayResultSpec: {
+            ____label: "Data Gateway Response Result Specification",
+            ____accept: "jsObject",
+            ____defaultValue: { ____accept: "jsObject" }
+        },
+        gatewayErrorContextSpec: {
+            ____label: "Data Gateway Response Error Specification",
+            ____accept: "jsObject",
+            ____defaultValue: {
+                ____types: "jsObject",
+                error_message: { ____accept: "jsString" },
+                source_tag: { ____accept: "jsString" }
+            }
         },
         gatewayMessageHandler: {
             ____label: "Data Gateway Message Handler",
@@ -41,6 +52,13 @@ var factoryResponse = arccore.filter.create({
         var inBreakScope = false;
         while (!inBreakScope) {
             inBreakScope = true;
+
+            // Create a filter to be used to validate/normalize a non-error result response value.
+
+            // Create a filter to be used to validate/normalize an error context object 
+
+
+
             var innerFactoryResponse = arccore.filter.create({
                 operationID: factoryRequest_.id,
                 operationName: factoryRequest_.name + " Data Gateway Filter",
@@ -54,7 +72,11 @@ var factoryResponse = arccore.filter.create({
                     },
                     gatewayMessage: factoryRequest_.gatewayMessageSpec
                 },
-                bodyFunction: factoryRequest_.gatewayMessageHandler
+                bodyFunction: function(request_) {
+
+                    return factoryRequest_.gatewayMessageHandler(request_);
+                }
+
             });
             if (innerFactoryResponse.error) {
                 errors.push(innerFactoryResponse.error);
