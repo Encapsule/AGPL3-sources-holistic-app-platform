e8babe8f26c967abf2c7a60037c0885072acaa6f
Chris Russell <cdr@encapsule.io>
2019-11-02 08:23:37 -0700

    WIP: Working to stabilize the output of the OPC constructor... filter that is responsible for building opci._private descriptor that is a dependency of all OPC class methods. I had an output filter spec in place to pass the rough shape of this output through until I was fairly certain it was basically stable. Now, I am completing the output spec down to exacting details of the structures created by the constructor filter. It's really critical that the evaluation method in particular only deal with opci._private in a very tightly controlled and unit tested way. Otherwise, there are just too many layers of possible error to reasonably debug.

--- SOURCES/LIB/holarchy/opc/filters/iospecs/opc-method-constructor-output-spec.js
+++ SOURCES/LIB/holarchy/opc/filters/iospecs/opc-method-constructor-output-spec.js
@@ -3,17 +3,47 @@
 
 module.exports = {
     ____types: "jsObject",
-    id: { ____accept: "jsString" },
-    iid: { ____accept: "jsString" },
-    name: { ____accept: "jsString" },
-    description: { ____accept: "jsString" },
+
+    id: {
+        ____label: "OCP System Version-Independent ID",
+        ____description: "Unique developer-assigned Version-Independent Identifier (VIID) that does not changed. Used to grossly discriminate different OPC systems that may be used in one runtime. And, in logs.",
+        ____accept: "jsString"
+    },
+
+    iid: {
+        ____label: "OCP System Instance ID",
+        ____description: "Random v4 UUID-derived IRUT used to identify this specific OPC instance.",
+        ____accept: "jsString"
+    },
+
+    name: {
+        ____label: "OCP System Name",
+        ____description: "Developer-defined short name assigned to this OPC system model.",
+        ____accept: "jsString"
+    },
+
+    description: {
+        ____label: "OCP System Description",
+        ____description: "Developer-defined short descripion of the function and/or role of this OPC configuration.",
+        ____accept: "jsString"
+    },
+
     opmMap: { ____accept: "jsObject" },
+
     opmiSpecPaths: { ____accept: "jsArray" },
+
     ocdSpec: { ____accept: "jsObject" },
+
     ocdi: { ____accept: "jsObject" },
+
     transitionDispatcher: { ____accept: "jsObject" },
+
     actionDispatcher: { ____accept: "jsObject" },
+
     evalCount: { ____accept: "jsNumber", ____defaultValue: 0 },
+
     lastEvalResponse: { ____accept: [ "jsObject", "jsNull" ], ____defaultValue: null },
+
     opcActorStack: { ____accept: "jsArray", ____defaultValue: [] }
+
 };
