9bb3fb7f2b8ae384fb6d3d053bc167711de898a1
Chris Russell <cdr@encapsule.io>
2019-11-14 21:01:55 -0800

    WIP: Split filter specs out of holodeck runner.

---
+++ SOURCES/LIB/holodeck/lib/iospecs/holodeck-runner-input-spec.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+    ____types: "jsObject",
+    id: {
+        ____accept: "jsString",
+    },
+    name: {
+        ____accept: "jsString"
+    },
+    description: {
+        ____accept: "jsString"
+    },
+    logsRootDir: {
+        // Fully-qualified local directory where the test runner will create JSON-format log files.
+        ____accept: "jsString"
+    },
+    testHarnessFilters: {
+        ____types: "jsArray",
+        ____defaultValue: [],
+        testHarnessFilter: { ____accept: "jsObject" } // test harness filter instance reference
+    },
+    testRequestSets: {
+        ____types: "jsArray",
+        ____defaultValue: [],
+        testRequestSet: {
+            ____types: "jsArray",
+            testRequest: {
+                ____types: [ "jsUndefined" , "jsObject" ],
+                id: { ____accept: "jsString" },
+                name: { ____accept: "jsString" },
+                description: { ____accept: "jsString" },
+                // expectedOutcome: { ____accept: "jsString", ____inValueSet: [ "pass", "fail" ] },
+                harnessRequest: { ____accept: [ "jsUndefined", "jsObject" ] }
+            }
+        }
+    }
+};
