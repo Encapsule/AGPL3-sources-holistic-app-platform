157eb7a79b016827d0fb2e5d2300128145b13474
Chris Russell <cdr@encapsule.io>
2019-12-03 14:10:51 -0800

    WIP: Update holdeck baseline.

--- TESTS/holarchy-package-tests/fixture-act-examples.js
+++ TESTS/holarchy-package-tests/fixture-act-examples.js
@@ -4,9 +4,22 @@ const holarchy = require("@encapsule/holarchy");
 module.exports = [
 
     new holarchy.ControllerAction({
+        id: "BO184bcvSzmr9oF4KJynSA",
+        name: "Bogus Controller Test Action #1",
+        description: "Do nothing nothing test controller action plug-in filter #2.",
+        actionRequestSpec: {
+            ____types: "jsObject",
+            noop2: {
+                ____accept: "jsBoolean",
+                ____inValueSet: [ true ]
+            }
+        }
+    }),
+
+    new holarchy.ControllerAction({
         id: "93_stDeORb-9W_0d69fswg",
-        name: "NOOP Controller Test Action #1",
-        description: "Do nothing test controller action plug-in filter #1.",
+        name: "NOOP Controller Test Action #2",
+        description: "Do nothing test controller action plug-in filter #2.",
         actionRequestSpec: {
             ____types: "jsObject",
             noop1: {
@@ -22,16 +35,21 @@ module.exports = [
 
     new holarchy.ControllerAction({
         id: "BO184bcvSzmr9oF4KJynSA",
-        name: "NOOP Controller Test Action #2",
-        description: "Do nothing nothing test controller action plug-in filter #2.",
+        name: "NOOP Controller Test Action #3",
+        description: "Do nothing nothing test controller action plug-in filter #3.",
         actionRequestSpec: {
             ____types: "jsObject",
             noop2: {
                 ____accept: "jsBoolean",
                 ____inValueSet: [ true ]
             }
+        },
+        bodyFunction: function(request_) {
+            return { error: null, result: false };
         }
-    })
+    }),
+
+
 
 ];
 
