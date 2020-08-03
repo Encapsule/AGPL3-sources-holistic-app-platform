19fd0cf8f5d06c507f8c794c19bead7b10f1285f
Chris Russell <cdr@encapsule.io>
2020-08-03 15:01:41 -0700

    WIP: Preliminary implementation of cell process manager ... ... create process. This isn't tested yet. Dropping off for a bit to guy buy a Pikes Peak Multistrada!

--- SOURCES/LIB/holarchy/lib/intrinsics/CellProcessor/ControllerAction-cpm-process-create.js
+++ SOURCES/LIB/holarchy/lib/intrinsics/CellProcessor/ControllerAction-cpm-process-create.js
@@ -1,6 +1,9 @@
 // SOURCES/LIB/holarchy/lib/intrinsics/ControllerAction-cpm-process-create.js
 
+const arccore = require("@encapsule/arccore");
 const ControllerAction = require("../../ControllerAction");
+const cpmMountingNamespaceName = require("../../filters/cpm-mounting-namespace-name");
+
 
 const controllerAction = new ControllerAction({
     id: "SdL0-5kmTuiNrWNu7zGZhg",
@@ -18,7 +21,8 @@ const controllerAction = new ControllerAction({
                     create: {
                         ____types: "jsObject",
                         apmID: { ____accept: "jsString" },
-                        initData: { ____opaque: true }
+                        cellProcessUniqueName: { ____accept: [ "jsUndefined", "jsString" ] },
+                        cellProcessInitData: { ____accept: "jsObject", ____defaultValue: {} }
                     }
                 }
             }
@@ -27,8 +31,8 @@ const controllerAction = new ControllerAction({
 
     actionResultSpec: {
         ____types: "jsObject",
-        apmBindingPath: { ____accept: "jsString" }, // this is the OCD path of the new process
-        cellProcessID: { ____accept: "jsString" } // this is an IRUT-format hash of the apmBindingPath
+        apmBindingPath: { ____accept: [ "jsNull", "jsString" ], ____defaultValue: null }, // this is the OCD path of the new process
+        cellProcessID: { ____accept: [ "jsNull", "jsString"], ____defaultValue: null } // This is an IRUT-format per-CellProcessor-instance-unique ID
     },
 
     bodyFunction: function(request_) {
@@ -38,6 +42,47 @@ const controllerAction = new ControllerAction({
         while (!inBreakScope) {
             inBreakScope = true;
             console.log("Cell Process Manager process create...");
+
+            const message = request_.actionRequest.holarchy.CellProcessor.process.create;
+
+            const apmProcessesNamespace = `~.${message.apmID}_CellProcesses`;
+
+            let ocdResponse = request_.context.ocdi.getNamespaceSpec(apmProcessesNamespace);
+            if (ocdResponse.error) {
+                errors.push(`Invalid apmID value '${message.apmID}' specified. No CellModel registered in this CellProcessor based on this AbstractProcessModel.`);
+                errors.push(ocdResponse.error);
+                break;
+            }
+
+            // Calculate the IRUT-format hash of the caller's specified cellProcessUniqueName. Or, if not specified use a IRUT-format v4 UUID instead.
+            // The implication here is test vector log stability in holodeck primarily:
+            // So, I recommend but do not require that derived apps / services always specifiy cellProcessUniqueName value.
+
+            let apmProcessInstanceID =
+                (message.cellProcessUniqueName?
+                 arccore.identifier.irut.fromReference(message.cellProcessUniqueName).result
+                 :
+                 arccore.identifier.irut.fromEther().result
+                );
+
+
+            const apmBindingPath = `${apmProcessesNamespace}.${apmProcessInstanceID}`;
+            const newCellProcessID = arccore.identifier.irut.fromReference(apmBindingPath).result;
+
+            ocdResponse = request_.context.ocdi.getNamespaceSpec(apmBindingPath);
+            if (!ocdResponse.error) {
+                errors.push(`Invalid cellProcessUniqueName value '${message.cellProcessUniqueName}' is not unique; cell process already exists.`);
+                break;
+            }
+
+            ocdResponse.writeNamespace(apmBindingPath, message.cellProcessInitData);
+            if (ocdResponse.error) {
+                errors.push(`Failed to create cell process at OCD path '${newProcessNamespace}' due to problems with the process initialization data specified.`);
+                errors.push(ocdResponse.error);
+            }
+
+            response.result = { apmBindingPath: newProcessNamespace, cellProcessID };
+
             break;
         }
         if (errors.length) {
