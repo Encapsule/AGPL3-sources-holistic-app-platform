7450de40e66ae902812a3e873d64a6b74866408d
Chris Russell <cdr@encapsule.io>
2020-10-08 23:03:29 -0700

    WIP Stash incremental work on CellProcessProxy virtualization in OCD implementation.

--- SOURCES/LIB/holarchy/lib/filters/get-namespace-in-reference-from-path.js
+++ SOURCES/LIB/holarchy/lib/filters/get-namespace-in-reference-from-path.js
@@ -30,6 +30,37 @@ const arccore = require("@encapsule/arccore");
                 ____description: "A reference to the source filter specification descriptor object. Iff dataRef === undefined, then this filter returns the filter spec namespace descriptor object reference corresponding to namespacePath.",
                 ____accept: "jsObject" // This filter presumes specRef to be a reference to a valid filter spec namespace descriptor object (or object tree). But, it does not itself validate this presumption.
                 // ____defaultValue: {} // TODO: Remove this --- it's not optional that the caller specify specRef. We're just not quite there yet.
+            },
+
+            cellProxyOptions: {
+                ____label: "Proxy Resolution Option",
+                ____description: "Options descriptor object considered only when both dataRef and specRef are specified (i.e. this filter is attempting to resolve a data namespace reference from namespacePath).",
+                ____types: "jsObject",
+                ____defaultValue: {},
+                resolveMode: {
+                    ____label: "Proxy Resolve Mode",
+                    ____description: "The only OCD client that should ever override the default value is CellProcessProxy CellModel implementation. Everyone else should always take the default value.",
+                    ____accept: "jsString",
+                    ____inValueSet: [
+                        "disable-proxy-resolution",      // Ignore CellProcessProxy helper cell instances and perform low-level mechanical resolution of the requested data namespace reference from namespacePath.
+                        "resolve-intermediate-proxies",  // Detect CellProcessProxy helper cell instances and resolve through intermediate-level proxies (i.e. encountered during the search) iff connected. Disable proxy resolution on last path token of the search.
+                        "resolve-entire-proxy-chain"     // Detect CellProcessProxy helper cell instances and resolve through all encountered proxy instances discovered in "connected" state (including the last path token in a search iff it's a connected proxy).
+                    ],
+                    ____defaultValue: "resolve-entire-proxy-chain" // WARNING: This override is provided explicitly for CellProcessProxy CellModel artifacts. No other client of OCD (direct or otherwise) should ever override this value.
+                },
+                indirectionCount: {
+                    ____label: "Proxy Indirection Count",
+                    ____description: "Never touch this. Not ever. It's only used by this filter itself to call itself when resolving a proxy.",
+                    ____accept: "jsNumber",
+                    ____defaultValue: 0
+                },
+                maxIndirections: {
+                    ____label: "Max Proxy Indirections",
+                    ____description: "The maximum number of connected CellProcessProxy instances that may be resolved in a single request. In general, it's rare to use more than 2-3 levels of proxy indirection. So, hitting the max limit typically indicates an unresolvable cycle in proxy connections.",
+                    ____accept: "jsNumber",
+                    ____inRangeInclusive: { begin: 0, end: 16 },
+                    ____defaultValue: 8
+                }
             }
         },
 
@@ -38,15 +69,19 @@ const arccore = require("@encapsule/arccore");
         },
 
         bodyFunction: function(request_) {
+
             let response = { error: null, result: null };
             let errors = [];
             let inBreakScope = false;
+
             while (!inBreakScope) {
                 inBreakScope = true;
-                let proxyIndirects = 0;
+
+                let proxyIndirectCount = 0;
                 let resolvedPathTokens = [];
                 let resolvedSpecPathTokens = [];
                 let pathTokens = request_.namespacePath.split(".");
+
                 if (pathTokens[0] !== "~") {
                     errors.push(`Invalid request.namespacePath value '${request_.namespacePath}' specified.`);
                     errors.push("Path must be a an ARCcore.filter-style dot-delimited path beginning with ~ that is taken to represent the value passed via request.dataRef.");
@@ -57,12 +92,16 @@ const arccore = require("@encapsule/arccore");
                     response.result = request_.dataRef?request_.dataRef:request_.specRef;
                     break;
                 }
+
                 const currentToken = pathTokens.shift();
                 resolvedPathTokens.push(currentToken);
                 resolvedSpecPathTokens.push(currentToken);
                 let dataRef = request_.dataRef;
                 let specRef = request_.specRef;
                 const dataRefQuery = dataRef?true:false;
+
+                console.log(`> OCD.getNamespaceInReferenceFromPath ${dataRefQuery?"store data":"store specification"} search on OCD namespace path '${request_.namespacePath}' starting...`);
+
                 while (pathTokens.length) {
                     let resolveFilterSpecContainerElementDescriptor = false;
                     if (specRef.____asMap) {
@@ -96,6 +135,7 @@ const arccore = require("@encapsule/arccore");
                         errors.push("But, '" + resolvedPath + "' is actually an \"" + actualType + "\" entity type that does not have subnamespaces.");
                         break;
                     }
+
                     if (dataRefQuery) {
                         innerResponse = arccore.types.check.inTypeSet({
                             types: [ "jsObject", "jsArray" ],
@@ -114,6 +154,7 @@ const arccore = require("@encapsule/arccore");
                             break;
                         }
                     }
+
                     let token = pathTokens.shift();
                     let specToken = token;
                     if (resolveFilterSpecContainerElementDescriptor) {
@@ -137,31 +178,54 @@ const arccore = require("@encapsule/arccore");
 
                         // Special-case handling for data reference resolution via CellProcessProxy (APM) bound helper cells encountered in the data.
 
-                        if (specRef.____appdsl && specRef.____appdsl.apm && (specRef.____appdsl.apm === "CPPU-UPgS8eWiMap3Ixovg" /*"Holarchy Cell Process Proxy Helper Process"*/)) {
-                            console.log(`> Currently examining specRef path '${resolvedSpecPathTokens.join(".")}' that declared as a CellProcessProxy helpers cell.`);
+                        if (request_.cellProxyOptions.resolveMode !== "disable-proxy-resolution") {
 
-                            let currentNamespaceIsQueryTarget = false;
-                            let nextTokenInProxySpec = false;
-                            let proxyIsConnected = false;
+                            // Determine if the current namespace has an ____appdsl.apm === CellProcessProxy APM ID.
+                            if (specRef.____appdsl && specRef.____appdsl.apm && (specRef.____appdsl.apm === "CPPU-UPgS8eWiMap3Ixovg" /*"Holarchy Cell Process Proxy Helper Process"*/)) {
 
-                            
+                                console.log(`> OCD::getNamespaceInReferenceFromPath is currently examining specRef path '${resolvedSpecPathTokens.join(".")}' that is declared as a CellProcessProxy helpers cell instance.`);
 
-                        }
+                                const endOfSearch = (pathTokens.length === 0);
 
+                                if (endOfSearch) {
+                                    // The search has ended on a CellProcessProxy helper cell. Figure out how to respond.
+
+                                    if (request_.cellProxyOptions.resolveModel === "resolve-intermediate-proxies") {
+                                        // We're done. dataRef is what the caller has asked for.
+                                        console.log(`..... Returning data reference to CellProcessProxy instance memory at path '${resolvedPathTokens.join(".")}'.`);
+                                    } else {
+                                        console.log(`..... We think we should now complete this request by attempting to resolve through the proxy '${resolvedPathTokens.join(".")}' in order to return a reference to the connected cell process.`);
+                                    }
+                                } else {
+                                    // The search has encounted a CellProcessProxy helper cell while the search is in progress.
+                                    console.log(`..... We think we should now attempt to resolve through the proxy '${resolvedPathTokens.join(".")}' and then complete the search using remain path tokens applied to updated dataRef and specRef.`);
+                                } // end if !endOfSearch
+
+                            } // if current data namespace is a CellProcessProxy helper cell.
+
+                        } // if !resolveMode !== "disable-proxy-resolution"
+
+                    } else {
+                        // Keep the resolvedPathTokens up-to-date even if we're resolving a filter spec descriptor; we need this to report error in terms of the namespacePath value supplied by the caller.
+                        resolvedPathTokens.push(token);
                     }
 
                 } // while pathTokens.length
                 if (errors.length) {
                     break;
                 }
+
                 if (!request_.dataRef && (Object.prototype.toString.call(specRef) !== "[object Object]")) {
-                    errors.push(`Cannot resolve the specified OCD path '${request_.namespacePath}' to the namespace's corresponding filter specification because the data path you provided is wrong.`);
-                    errors.push(`Specifically, the last namespace in the path '${resolvedPathTokens.join(".")}' is not declared in this OCD instance's controlling filter specification.`);
+                    errors.push(`Cannot resolve the specified OCD path '${request_.namespacePath}' to the namespace's corresponding filter specification because`);
+                    errors.push(`the last namespace in the path '${resolvedPathTokens.join(".")}' is not declared in this OCD instance's filter specification.`);
                     break;
                 }
+
                 response.result = request_.dataRef?dataRef:specRef;
                 break;
+
             }
+
             if (errors.length) {
                 response.error = errors.join(" ");
             }
