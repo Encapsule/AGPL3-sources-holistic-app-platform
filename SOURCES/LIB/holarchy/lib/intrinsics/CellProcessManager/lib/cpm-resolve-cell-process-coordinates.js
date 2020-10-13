d4a3a6dcb30ed9cd7339dd3c40bf218bc5096be4
Chris Russell <cdr@encapsule.io>
2020-10-13 07:48:00 -0700

    Improve caching performance of cellProcessCoordinates resolver filter in cpmLib. Under this debugger the result cache has the interesting property of serving as an overall record of all the coordinates in the cell process plane that have been activated in the context of the OS process (note that the result cache is per module and our Node.js and emulated Node.js module loading (via webpack currently) ensures that every module is a singleton. Thus, when we execute the entire platform regression test suite under the debugger and observe the result cache we see that even despite the fact that we're loading many different CellProcessor instances under test and exercising many different CellModel combinations (often we share CellModel's across tests), we can see the overall result cache as a high-level activity record This is potentially very useful later.

--- SOURCES/LIB/holarchy/lib/intrinsics/CellProcessManager/lib/cpm-resolve-cell-process-coordinates.js
+++ SOURCES/LIB/holarchy/lib/intrinsics/CellProcessManager/lib/cpm-resolve-cell-process-coordinates.js
@@ -36,26 +36,39 @@ const arccore = require("@encapsule/arccore");
             let inBreakScope = false;
             while (!inBreakScope) {
                 inBreakScope = true;
-                const cacheKey = `${request_.cellProcessCoordinates.apmID}::${request_.cellProcessCoordinates.instanceName}`;
-                if (!resultCache[cacheKey]) {
-                    const cpmXProcessesPath = `~.${request_.cellProcessCoordinates.apmID}_CellProcesses`;
-                    let ocdResponse = request_.ocdi.getNamespaceSpec(cpmXProcessesPath);
+
+
+                if (!resultCache[request_.cellProcessCoordinates.apmID]) {
+
+                    const cellProcessesPath = `~.${request_.cellProcessCoordinates.apmID}_CellProcesses`;
+                    let ocdResponse = request_.ocdi.getNamespaceSpec(cellProcessesPath);
                     if (ocdResponse.error) {
                         errors.push(`Cell process coordinates cannot be resolved to CPM process path/ID because the APM specified, ID '${request_.cellCoordinates.apmID}', is not known inside this CellProcessor instance. Error detail:`);
                         errors.push(ocdResponse.error);
                         break;
                     }
+                    resultCache[request_.cellProcessCoordinates.apmID] = { cellProcessesPath, instances: {} };
+
+                } // if APM ID cache miss
+
+                if (!resultCache[request_.cellProcessCoordinates.apmID].instances[request_.cellProcessCoordinates.instanceName]) {
+
+                    const cellProcessesPath = resultCache[request_.cellProcessCoordinates.apmID].cellProcessesPath;
+
                     const cellProcessInstanceIdentifier = arccore.identifier.irut.fromReference(request_.cellProcessCoordinates.instanceName).result;
-                    const cellProcessPath = `${cpmXProcessesPath}.cellProcessMap.${cellProcessInstanceIdentifier}`;
+                    const cellProcessPath = `${cellProcessesPath}.cellProcessMap.${cellProcessInstanceIdentifier}`;
                     const cellProcessID = arccore.identifier.irut.fromReference(cellProcessPath).result;
-                    resultCache[cacheKey] = {
+                    resultCache[request_.cellProcessCoordinates.apmID].instances[request_.cellProcessCoordinates.instanceName] = {
                         cellProcessCoordinates: request_.cellProcessCoordinates,
-                        cellProcessesPath: cpmXProcessesPath,
+                        cellProcessesPath,
                         cellProcessPath,
                         cellProcessID
                     };
-                } // if resultCache miss
-                response.result = resultCache[cacheKey];
+                } // if instanceName cache miss
+
+                // Return the cached result.
+                response.result = resultCache[request_.cellProcessCoordinates.apmID].instances[request_.cellProcessCoordinates.instanceName];
+
                 break;
             }
             if (errors.length) {
