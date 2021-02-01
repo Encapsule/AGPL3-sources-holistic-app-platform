0d77d50240d35f18fd5df45ec9008e98a9c600ac
Chris Russell <cdr@encapsule.io>
2021-02-01 08:53:30 -0800

    Improvements to find-irut.js utility script to make it a cli.

--- scraps/find-irut.js
+++ scraps/find-irut.js
@@ -1,17 +1,33 @@
-// require("../TESTS/mock-platform");
+#!/usr/bin/env node
 
 const arccore = require("@encapsule/arccore");
+const process = require("process");
 
-const prefix = "CPP";
-const testLimit = 1000000;
-let testCount = 0;
+const prefix = process.argv[2]?process.argv[2]:null;
+let testLimit = process.argv[3]?process.argv[3]:"5000";
+
+if ((testLimit < 0) || (testLimit > 1000000)) {
+    console.error(`Invalid iterationLimit "${testLimit}" specified .`);
+    process.exitCode = 1;
+    return;
+}
+
+if (prefix && (prefix.length > 4)) {
+    console.log(`Invalid prefeix "${prefix}" exceed reasonable character length. You will never find it...`);
+    process.exitCode = 1;
+    return;
+}
+
+console.log(`>>>> Searching for IRUT beginning in "${prefix}" for max of ${testLimit} random IRUTs...`);
 
 let result = null;
+let testCount = 0;
+
 while (!result && (testCount < testLimit)) {
     testCount++;
     const candidate = arccore.identifier.irut.fromEther();
     console.log(testCount + " " + candidate);
-    if (candidate.startsWith(prefix)) {
+    if (!prefix || candidate.startsWith(prefix)) {
         result = candidate;
         break;
     }
@@ -19,7 +35,7 @@ while (!result && (testCount < testLimit)) {
 
 if (result) {
     console.log("FOUND:");
-    console.log(result);
+    console.log(`result = "${result}"`);
 } else {
     console.log("NO PREFIX MATCH FOUND in " + testLimit + " random IRUTS.");
 }
